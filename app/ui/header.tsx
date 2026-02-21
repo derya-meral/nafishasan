7107e5c876309f24e7c1ac518ff87d915cff3c40
derya-meral <drderyameral@gmail.com>
2026-02-21 00:08:36 -0500

    fixed hamburger menu

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -18,21 +18,23 @@ export default function Header() {
                         htmlFor="menu-toggle"
                         className="peer-checked:hidden block"
                     >
-                        <Bars3Icon className="size-9 text-white rounded-md border-[1px] border-slate-600 self-center pl-1 pr-1 lg:hidden bg-slate-950" />
+                        <Bars3Icon className="size-9 text-white rounded-md border border-slate-600 self-center pl-1 pr-1 bg-slate-950" />
                     </label>
                     <label
                         htmlFor="menu-toggle"
                         className="peer-checked:block hidden"
                     >
-                        <XMarkIcon className="size-9 text-white rounded-md border-[1px] border-slate-600 self-center pl-1 pr-1 lg:hidden bg-slate-950" />
+                        <XMarkIcon className="size-9 text-white rounded-md border border-slate-600 self-center pl-1 pr-1 bg-slate-950" />
                     </label>
-                    <div className="hidden peer-checked:flex peer-checked:flex-col peer-checked:gap-3 absolute right-0 w-56 mt-1 border border-gray-500 rounded-xl p-5 bg-white text-right text-2xl font-mono text-black">
-                        <Link href="/">About</Link>
-                        <Link href="/metastasis">Metastasis</Link>
-                        <Link href="/writing">Writing</Link>
-                        <Link href="/speaking">Speaking</Link>
-                        <Link href="/teaching">Teaching</Link>
-                        <Link href="/#contact">Contact</Link>
+                    <div className="hidden peer-checked:block absolute right-0 w-56 mt-1">
+                        <div className="flex flex-col gap-3  border border-gray-500 rounded-xl p-5 bg-white text-right text-2xl font-mono text-black">
+                            <Link href="/">About</Link>
+                            <Link href="/metastasis">Metastasis</Link>
+                            <Link href="/writing">Writing</Link>
+                            <Link href="/speaking">Speaking</Link>
+                            <Link href="/teaching">Teaching</Link>
+                            <Link href="/#contact">Contact</Link>
+                        </div>
                     </div>
                 </div>
             </div>
